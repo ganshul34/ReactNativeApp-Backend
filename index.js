6999fa74ce11fa3856fd8b81396aca3b5cbbca86
Anshul <ganshul34@gmail.com>
2020-01-05 01:32:26 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb+srv://root:toor@cluster0-gjn0o.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true }).then(
+    () => {console.log(`Database is connected`) },
+    err => { console.log('Can not connect to the database'+ err)}
+  );
+
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`app running on ${PORT}`);
+});
+
+
